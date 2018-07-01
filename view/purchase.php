53e92c415a775762e98d9a4eb01ac8fc6163ecd4
Mohamed Haseem <haseem.ucsc@gmail.com>
2018-07-02 00:02:15 +0530

    Try to code autocompleate

--- view/purchase.php
+++ view/purchase.php
@@ -6,10 +6,12 @@ if (!isset($_SESSION['uid'])){
 	exit;
 }
 require_once '../config/dbconn.php';
+/* This php code is for pass the item id to $item_id_array. */
+
 /* $result=mysqli_query($conn, "SELECT DISTINCT item_id from item ORDER BY item_id");
 while ($row = mysqli_fetch_assoc($result)) {
-	$item_id_array[]=  $row['item_id']; 
-}*/
+	$item_id_array[]=  $row['item_id'];  */
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -238,7 +240,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 		<div id="nav-right">
 
 			<div class="nav-divider">&nbsp;</div>
-			<a href="../config/logout.php" id="logout"><img src="images/shared/nav/nav_logout.gif" width="64" height="14" alt="" /></a> 
+			<a href="../config/logout.php" id="logout"><img src="../assets/images/shared/nav/nav_logout.gif" width="64" height="14" alt="" /></a> 
 			<div class="clear">&nbsp;</div>
 
 		</div>
@@ -271,12 +273,12 @@ while ($row = mysqli_fetch_assoc($result)) {
 <!--form action="../model/VoucherModel.php" method="post"-->
 		<form id="">
 		<div id="ItemTableForm">
-		<input type="text" id="Item-code"  class="form-control" onfocus="this.value = ''" onkeypress="additems(event)" placeholder="Enter Item Code" />
+		<input type="text" id="Item-code"  class="form-control" onfocus="this.value = ''"  placeholder="Enter Item Code" onkeypress="additems(event)" autocomplete="on" />
 		<input type="text" id="Suplier-name"  class="form-control" placeholder="Enter Suplier Name" required />
 		</div>
 
 		<table id="ItemTable" border="1">
-		<!-- Rohan grand total not working if I put the heading so I cometed the line please correct the error and erace comment -->
+		<!-- Rohan grand total not working if I put the heading so I cometed the line please correct this error -->
 		
 			<!--tr><th width="25%">Item Code</th><th width="25%">Price</th><th width="25%">Quantity</th><th width="25%">Total</th></tr-->
 			
@@ -309,6 +311,115 @@ while ($row = mysqli_fetch_assoc($result)) {
         </table>
 		</div>	
 		</form>
+		
+		
+	<!--script> Rohan this Javascript code for auto compleate list, this list is feeded by $item_id_array created by php you can find the php code for this in line no 9 this code is working my another project but not working here!! I don't know the reson. Its better if you fix this also, Now I have enabled autocomplete="on" attribute for get the item code from browser cache. but the item code should come from database rather than browser cache. --->
+		
+	<!--script> 
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+      });
+}
+var countries = <?php echo json_encode($item_id_array); ?>;
+
+autocomplete(document.getElementById("Item-code"), countries);
+</script-->		
+		
+		
+		
+		
     </body>
 
 
